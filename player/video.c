649f04046a4cef0d9d5068ac7a3e475bd69a1bbf
xnoreq <xnoreq@gmail.com>
2014-03-21 12:46:06 +0100

    fixed A-V desync on playback speed reset

--- player/video.c
+++ player/video.c
@@ -392,7 +392,7 @@ double update_video(struct MPContext *mpctx, double endpts)
 			framedrop_type = 1;
 		else if (opts->frame_dropping && mpctx->dropped_frames >= 0
 				&& frames_lag > 1
-				&& mpctx->dropped_frames*mpctx->last_frame_time / opts->playback_speed < 1.0) {
+				&& mpctx->dropped_frames*(mpctx->last_frame_time/opts->playback_speed) < 1.0) {
 			// drop as long as we lag more than we can skip
 			// but ensure a minimum of 1.0 fps
 			framedrop_type = opts->frame_dropping;
@@ -400,7 +400,7 @@ double update_video(struct MPContext *mpctx, double endpts)
 			mpctx->drop_frame_cnt++;
 			
 			// update pts so that we can update last_av_difference
-			mpctx->video_pts += mpctx->last_frame_time / opts->playback_speed;
+			mpctx->video_pts += mpctx->last_frame_time;
 			mpctx->last_vo_pts = mpctx->video_pts;
 			mpctx->playback_pts = mpctx->video_pts;
 		} else {
@@ -455,7 +455,7 @@ double update_video(struct MPContext *mpctx, double endpts)
 		mpctx->last_frame_time = frame_time;
 		
 		// skip as long as we lag but ensure a minimum of 20 fps
-		if (frames_lag && mpctx->skipped_frames*mpctx->last_frame_time / opts->playback_speed < 0.05) {
+		if (frames_lag && mpctx->skipped_frames*(mpctx->last_frame_time/opts->playback_speed) < 0.05) {
 			vo_skip_frame(video_out);
 			mpctx->skip_frame_cnt++;
 			mpctx->skipped_frames++;
@@ -463,7 +463,6 @@ double update_video(struct MPContext *mpctx, double endpts)
 			mpctx->video_pts = mpctx->video_next_pts;
 			mpctx->last_vo_pts = mpctx->video_pts;
 			mpctx->playback_pts = mpctx->video_pts;
-			return 0;
 		} else
 			mpctx->skipped_frames = 0;
 	}
